c9beee04100cd1e42156fd659312f7c033e3226c
richianbalaido27 <richianbalaido27@gmail.com>
2025-02-14 13:06:11 +0800

    index.html
    FLowers

---
+++ script.js
@@ -0,0 +1,23 @@
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+  
+      const titles = ('FLOWERS FOR YOU BEBILYN').split('')
+      
+      const titleElement = document.getElementById('title');
+      let index = 0;
+  
+      function appendTitle() {
+        if (index < titles.length) {
+          titleElement.innerHTML += titles[index];
+          index++;
+          setTimeout(appendTitle, 300); 
+        }
+      }
+  
+      appendTitle();
+  
+      clearTimeout(c);
+    }, 1000);
+  };
